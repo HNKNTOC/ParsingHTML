8adfded8e431b5efd3c3e15781d44c08864f1359
Nikita <nikita.kamyshenko@mail.ru>
2016-06-20 19:10:04 +0300

    setting

--- src/main/java/com/parsingHTML/logic/Main.java
+++ src/main/java/com/parsingHTML/logic/Main.java
@@ -26,34 +26,17 @@ import java.io.IOException;
  * Created by Nikita on 03.05.2016.
  */
 public class Main {
-    public static void main(String[] args) throws TransformerException, ParserConfigurationException, IOException {newStart();
+    public static void main(String[] args) throws TransformerException, ParserConfigurationException, IOException {
         start();
     }
 
     private static XMLFactory XMLFactory = new ElementFactoryJsoup();
 
-    private static void newStart() throws ParserConfigurationException, TransformerException {
-        Document documentDom = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-
-        Element element = new Element(Tag.valueOf("testtag"),"");
-        element.appendChild(new Element(Tag.valueOf("lol"),""));
-
-        transformation(documentDom, element);
-
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        Result output = new StreamResult(new File("src\\main\\resources\\test\\output.xml"));
-        Source input = new DOMSource(documentDom);
-
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-        transformer.transform(input, output);
-    }
-
     private static void transformation(Document documentDom, Element element) {
         org.jsoup.nodes.Document document = new org.jsoup.nodes.Document("");
         W3CDom w3CDom = new W3CDom();
         document.appendChild(element);
-        w3CDom.convert(document,documentDom);
+        w3CDom.convert(document, documentDom);
     }
 
     private static void start() throws IOException, TransformerException {
@@ -72,7 +55,7 @@ public class Main {
         root.appendChild(groupLesson);
 
         Document doc = createDOC();
-        transformation(doc,schedule);
+        transformation(doc, schedule);
 
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         Result output = new StreamResult(new File("src\\main\\resources\\test\\output.xml"));
@@ -102,16 +85,16 @@ public class Main {
     }
 
 
-
-    public static File getFile(String name){
+    public static File getFile(String name) {
         return new FileManagerDefault().getFile(name);
     }
 
     /**
      * Загрузка html
+     *
      * @param stringURL
      */
-    public static void loadHTML(String stringURL){
+    public static void loadHTML(String stringURL) {
         LoaderHTML loaderHTMLDefault = new LoaderHTMLDefault();
         File file = loaderHTMLDefault.loadHTML("http://rasp.bukep.ru/");
     }
