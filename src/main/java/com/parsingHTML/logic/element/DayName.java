664928a49a4ba6fa3d1f922ddd17d643af85cd3b
Nikita <nikita.kamyshenko@mail.ru>
2016-08-20 23:25:06 +0300

    Add nameShort for DayName.

--- src/main/java/com/parsingHTML/logic/element/DayName.java
+++ src/main/java/com/parsingHTML/logic/element/DayName.java
@@ -4,22 +4,35 @@ package com.parsingHTML.logic.element;
  * Дни недели.
  */
 public enum DayName {
-    MONDAY("Понедельник"), TUESDAY("Вторник"), WEDNESDAY("Среда"), THURSDAY("Четверг"), FRIDAY("Пятница"), SATURDAY("Суббота");
+    MONDAY("Понедельник", "Пн"),
+    TUESDAY("Вторник", "Вт"),
+    WEDNESDAY("Среда", "Ср"),
+    THURSDAY("Четверг", "Чт"),
+    FRIDAY("Пятница", "Пт"),
+    SATURDAY("Суббота", "Сб");
 
     private String nameRu;
+    private String nameShort;
 
-    DayName(String nameRu) {
+    DayName(String nameRu, String nameShort) {
         this.nameRu = nameRu;
+        this.nameShort = nameShort;
     }
 
     public String getNameRu() {
         return nameRu;
     }
 
-    @Override
-    public String toString() {
-        return nameRu;
+    public String getNameShort() {
+        return nameShort;
     }
 
+    @Override
+    public String toString() {
+        return "DayName{" +
+                "nameRu='" + nameRu + '\'' +
+                ", nameShort='" + nameShort + '\'' +
+                '}';
+    }
 }
 
