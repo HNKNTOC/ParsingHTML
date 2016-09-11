fa21d0e1940246cb023840f00868b1b4691ce073
Nikita <nikita.kamyshenko@mail.ru>
2016-09-11 17:39:53 +0300

    DayName add getName() and getNameShort()

--- src/main/java/com/parsingHTML/logic/element/DayName.java
+++ src/main/java/com/parsingHTML/logic/element/DayName.java
@@ -1,6 +1,8 @@
 package com.parsingHTML.logic.element;
 
+import java.text.DateFormatSymbols;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Дни недели.
@@ -19,12 +21,37 @@ public enum DayName {
      */
     private final int dayNumber;
 
+    private static DateFormatSymbols symbols = new DateFormatSymbols();
+    private static String[] shortWeekdays = symbols.getShortWeekdays();
+    private static String[] weekdays = symbols.getWeekdays();
+
     DayName(int dayNumber) {
         this.dayNumber = dayNumber;
     }
 
+    public static void setLocale(Locale locale) {
+        symbols = new DateFormatSymbols(locale);
+        updateDayName();
+    }
+
+    /**
+     * Обновление shortWeekdays и weekdays.
+     */
+    private static void updateDayName() {
+        shortWeekdays = symbols.getShortWeekdays();
+        weekdays = symbols.getWeekdays();
+    }
+
     public int getDayNumber() {
         return dayNumber;
+    }
+
+    public String getNameShort() {
+        return shortWeekdays[dayNumber];
+    }
+
+    public String getName() {
+        return weekdays[dayNumber];
     }
 
     @Override
--- src/test/java/com/parsingHTML/logic/element/DayNameTest.java
+++ src/test/java/com/parsingHTML/logic/element/DayNameTest.java
@@ -3,8 +3,10 @@ package com.parsingHTML.logic.element;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Тест для DayName.
@@ -20,6 +22,58 @@ public class DayNameTest {
         assertEquals(DayName.FRIDAY.getDayNumber(), Calendar.FRIDAY);
         assertEquals(DayName.SATURDAY.getDayNumber(), Calendar.SATURDAY);
         assertEquals(DayName.SUNDAY.getDayNumber(), Calendar.SUNDAY);
+    }
+
+    @Test
+    public void getNameShort() throws Exception {
+        DayName.setLocale(Locale.ENGLISH);
+        check(DayName.MONDAY.getNameShort(), "MON");
+        check(DayName.TUESDAY.getNameShort(), "TUE");
+        check(DayName.WEDNESDAY.getNameShort(), "WED");
+        check(DayName.THURSDAY.getNameShort(), "THU");
+        check(DayName.FRIDAY.getNameShort(), "FRI");
+        check(DayName.SATURDAY.getNameShort(), "SAT");
+        check(DayName.SUNDAY.getNameShort(), "SUN");
+    }
+
+    @Test
+    public void getName() throws Exception {
+        DayName.setLocale(Locale.ENGLISH);
+        check(DayName.MONDAY.getName(), "MONDAY");
+        check(DayName.TUESDAY.getName(), "TUESDAY");
+        check(DayName.WEDNESDAY.getName(), "WEDNESDAY");
+        check(DayName.THURSDAY.getName(), "THURSDAY");
+        check(DayName.FRIDAY.getName(), "FRIDAY");
+        check(DayName.SATURDAY.getName(), "SATURDAY");
+        check(DayName.SUNDAY.getName(), "SUNDAY");
+    }
+
+    @Test
+    public void getNameShortChangeLocale() throws Exception {
+        DayName.setLocale(new Locale("ru"));
+        check(DayName.MONDAY.getName(), "Понедельник");
+        check(DayName.TUESDAY.getName(), "Вторник");
+        check(DayName.WEDNESDAY.getName(), "Среда");
+        check(DayName.THURSDAY.getName(), "Четверг");
+        check(DayName.FRIDAY.getName(), "Пятница");
+        check(DayName.SATURDAY.getName(), "Суббота");
+        check(DayName.SUNDAY.getName(), "Воскресенье");
+    }
+
+    @Test
+    public void getNameChangeLocale() throws Exception {
+        DayName.setLocale(new Locale("ru"));
+        check(DayName.MONDAY.getNameShort(), "Пн");
+        check(DayName.TUESDAY.getNameShort(), "Вт");
+        check(DayName.WEDNESDAY.getNameShort(), "Ср");
+        check(DayName.THURSDAY.getNameShort(), "Чт");
+        check(DayName.FRIDAY.getNameShort(), "Пт");
+        check(DayName.SATURDAY.getNameShort(), "Сб");
+        check(DayName.SUNDAY.getNameShort(), "Вс");
+    }
+
+    public void check(String s1, String s2) {
+        assertTrue(s1, s1.equalsIgnoreCase(s2));
     }
 
 }
